abfef4040fee07ea50bb0247ee294bf26039260d
Kai Diefenbach <kai.diefenbach@iqpp.de>
2014-12-13 18:03:04 +0100

    Fixes path to template

--- views.py
+++ views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render_to_response
 from django.template import RequestContext
 
 
-def {{ app_name }}_view(request, template_name="{{ app_name }}.html"):
+def {{ app_name }}_view(request, template_name="{{ app_name }}/{{ app_name }}.html"):
     return render_to_response(template_name, RequestContext(request, {
 
     }))
