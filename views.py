f6cdbc81d8fbb67d885c966d9b7afecaf3a44401
Kai Diefenbach <kai.diefenbach@iqpp.de>
2014-12-13 14:48:31 +0100

    Added default view

--- views.py
+++ views.py
@@ -1,2 +1,8 @@
 from django.shortcuts import render_to_response
 from django.template import RequestContext
+
+
+def display_view(request, template_name):
+    return render_to_response(template_name, RequestContext({
+
+    }))
