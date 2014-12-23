01a2ad6e639f61528acc6fa455ed13866d3814b4
Kai Diefenbach <kai.diefenbach@iqpp.de>
2014-12-23 04:28:14 +0100

    Adds login/logout urls and templates.

--- urls.py
+++ urls.py
@@ -1,6 +1,10 @@
 from django.conf.urls import patterns, url
 
+urlpatterns = patterns('django.contrib.auth.views',
+    url('^login', "login", {"template_name": "{{ app_name }}/login.html"}, name='{{ app_name }}_login'),
+    url('^logout', "logout", {"template_name": "{{ app_name }}/logged_out.html"}, name='{{ app_name }}_logout'),
+)
 
-urlpatterns = patterns('{{ app_name }}.views',
+urlpatterns += patterns('{{ app_name }}.views',
     url(r'^$', "{{ app_name }}_view", name="{{ app_name }}_view"),
 )
